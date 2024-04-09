6dbd43f93b5fb5bd81d8a0b13b7c1a35b5f38031
Daniel <daniel.m.adr@hotmail.com>
2024-04-08 23:16:08 -0300

    first config

---
+++ src/main/java/com/andrade/NeuroChatIA/NeuroChatIaApplication.java
@@ -0,0 +1,13 @@
+package com.andrade.NeuroChatIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroChatIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuroChatIaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrade/NeuroChatIA/NeuroChatIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrade.NeuroChatIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroChatIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
